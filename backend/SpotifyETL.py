2e61ff892032768fbf8747ceaf7fe347d7a72170
samuelmoss <49079395+samuelmoss@users.noreply.github.com>
2020-05-01 12:32:25 -0500

    All Entities and App Complete

--- backend/SpotifyETL.py
+++ backend/SpotifyETL.py
@@ -68,21 +68,24 @@ class SpotifyETL:
         popularities = []
         durations = []
         datetime_played = []
+        explicit_flags = []
 
         try:
             for i in range(0, len(songs)):
                 ids.append(songs[i]['track']['id'])
                 names.append(songs[i]['track']['name'])
-                artist_ids.append(songs[i]['track']['album']['artists']['id'])
-                artists.append(songs[i]['track']['album']['artists']['name'])
+                artist_ids.append(songs[i]['track']['album']['artists'][0]['id'])
+                artists.append(songs[i]['track']['album']['artists'][0]['name'])
                 album_ids.append(songs[i]['track']['album']['id'])
                 albums.append(songs[i]['track']['album']['name'])
                 popularities.append(songs[i]['track']['popularity'])
                 durations.append(songs[i]['track']['duration_ms'])
                 datetime_played.append(songs[i]['played_at'])
+                explicit_flags.append(songs[i]['track']['explicit'])
 
             track_zipcols = list(
-                zip(ids, names, artist_ids, artists, album_ids, albums, popularities, durations, datetime_played))
+                zip(ids, names, artist_ids, artists, album_ids, albums, popularities, durations, datetime_played,
+                    explicit_flags))
 
         except Exception as e:
             logger.error(traceback.format_exc())
@@ -118,7 +121,6 @@ class SpotifyETL:
         valence = []
         tempo = []
         uri = []
-
         try:
             track_info_method = self.sp.audio_features(ids)
 
@@ -146,26 +148,252 @@ class SpotifyETL:
 
     def create_track_entity(self):
 
-        track_info = self.get_track_entity_data()
-        audio_features = self.get_track_audio_features()
+        try:
+            track_info = self.get_track_entity_data()
+            audio_features = self.get_track_audio_features()
 
-        info_df = pd.DataFrame(track_info, columns=['track_id', 'track_name', 'artist_id', 'artist_name', 'album_id',
-                                                    'album_name', 'track_popularity', 'track_duration',
-                                                    'datetime_played'])
+            info_df = pd.DataFrame(track_info,
+                                   columns=['track_id', 'track_name', 'artist_id', 'artist_name', 'album_id',
+                                            'album_name', 'track_popularity', 'track_duration',
+                                            'datetime_played', 'explicit'])
 
-        audio_df = pd.DataFrame(track_info, columns=['track_id', 'danceability', 'energy', 'key', 'loudness',
-                                                     'mode', 'speechiness', 'acousticness', 'liveness', 'valence',
-                                                     'tempo', 'uri'])
+            audio_df = pd.DataFrame(audio_features, columns=['track_id', 'danceability', 'energy', 'key', 'loudness',
+                                                         'mode', 'speechiness', 'acousticness', 'liveness', 'valence',
+                                                         'tempo', 'uri'])
 
-        track_entity = pd.merge(info_df,audio_df, how='inner')
-        track_entity['date_played'] = [datetime.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S.%fZ') for date_time_str
-                                  in track_entity['datetime_played']]
-        track_entity['date_played'] = [d.date() for d in track_entity['date_played']]
-        track_entity['time_played'] = [datetime.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S.%fZ') for date_time_str
-                                    in track_entity['datetime_played']]
-        track_entity['time_played'] = [timezone('UTC').localize(d) for d in track_entity['time_played']]
-        track_entity['time_played'] = [d.astimezone(timezone('US/Central')) for d in track_entity['time_played']]
-        track_entity['time_played'] = [d.time() for d in track_entity['time_played']]
-        track_entity['track_duration'] = track_entity['track_duration']/1000
+            track_entity = pd.merge(info_df, audio_df, how='inner')
+            track_entity['date_played'] = [datetime.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S.%fZ') for
+                                           date_time_str
+                                           in track_entity['datetime_played']]
+            track_entity['date_played'] = [d.date() for d in track_entity['date_played']]
+            track_entity['time_played'] = [datetime.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S.%fZ') for
+                                           date_time_str
+                                           in track_entity['datetime_played']]
+            track_entity['time_played'] = [timezone('UTC').localize(d) for d in track_entity['time_played']]
+            track_entity['time_played'] = [d.astimezone(timezone('US/Central')) for d in track_entity['time_played']]
+            track_entity['time_played'] = [d.time() for d in track_entity['time_played']]
+            track_entity['track_duration'] = track_entity['track_duration'] / 1000
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            print(e.args)
 
         return track_entity
+
+    def get_artist_ids(self):
+
+        try:
+            zippedlist = self.get_track_entity_data()
+            artist_id_list = []
+            for i in range(0, len(zippedlist)):
+                artist_id_list.append(zippedlist[i][2])
+
+            artist_id_set = set(artist_id_list)
+            unique_artist_ids = list(artist_id_set)
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            print(e.args)
+
+        return unique_artist_ids
+
+    def get_artist_data(self):
+
+        try:
+            artist_ids = self.get_artist_ids()
+            artist_method = self.sp.artists(artist_ids)
+            artists = artist_method['artists']
+
+            names = []
+            popularities = []
+            followers = []
+            image = []
+            genres = []
+            for i in range(0, len(artists)):
+                names.append(artists[i]['name'])
+                popularities.append(artists[i]['popularity'])
+                followers.append(artists[i]['followers']['total'])
+                image.append(artists[i]['images'][1]['url'])
+                genres.append(artists[i]['genres'])
+
+            artist_info_cols = list(
+                zip(artist_ids, names, popularities, followers, image, genres))
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            print(e.args)
+
+        return artist_info_cols
+
+    def create_artist_entity(self):
+
+        try:
+            artist_info = self.get_artist_data()
+            artist_df = pd.DataFrame(artist_info,
+                                     columns=['artist_id', 'artist_name', 'artist_popularity', 'artist_followers',
+                                              'artist_image', 'artist_genres'])
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            print(e.args)
+
+        return artist_df
+
+    def get_album_ids(self):
+
+        try:
+            zippedlist = self.get_track_entity_data()
+            album_id_list = []
+            for i in range(0, len(zippedlist)):
+                album_id_list.append(zippedlist[i][4])
+
+            album_id_set = set(album_id_list)
+            unique_album_ids = list(album_id_set)
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            print(e.args)
+
+        return unique_album_ids
+
+    def get_album_data(self):
+
+        try:
+            album_ids = self.get_album_ids()
+            if 20 < len(album_ids) <= 40:
+                composite_list = [album_ids[x:x + 20] for x in range(0, len(album_ids), 20)]
+                res1, res2 = zip(composite_list)
+                res1 = res1[0]
+                res2 = res2[0]
+                album_method = self.sp.albums(res1)
+                albums = album_method['albums']
+
+                names = []
+                popularities = []
+                tracks = []
+                images = []
+                release_dates = []
+                labels = []
+                artist_ids = []
+                artist_names = []
+                for i in range(0, len(albums)):
+                    names.append(albums[i]['name'])
+                    popularities.append(albums[i]['popularity'])
+                    tracks.append(albums[i]['total_tracks'])
+                    images.append(albums[i]['images'][1]['url'])
+                    release_dates.append(albums[i]['release_date'])
+                    labels.append(albums[i]['label'])
+                    artist_ids.append(albums[i]['artists'][0]['id'])
+                    artist_names.append(albums[i]['artists'][0]['name'])
+
+                album_method = self.sp.albums(res2)
+                albums = album_method['albums']
+
+                for i in range(0, len(albums)):
+                    names.append(albums[i]['name'])
+                    popularities.append(albums[i]['popularity'])
+                    tracks.append(albums[i]['total_tracks'])
+                    images.append(albums[i]['images'][1]['url'])
+                    release_dates.append(albums[i]['release_date'])
+                    labels.append(albums[i]['label'])
+                    artist_ids.append(albums[i]['artists'][0]['id'])
+                    artist_names.append(albums[i]['artists'][0]['name'])
+
+            elif len(album_ids) > 40:
+                composite_list = [album_ids[x:x + 20] for x in range(0, len(album_ids), 20)]
+                res1, res2, res3 = zip(composite_list)
+                res1 = res1[0]
+                res2 = res2[0]
+                res3 = res3[0]
+                album_method = self.sp.albums(res1)
+                albums = album_method['albums']
+
+                names = []
+                popularities = []
+                tracks = []
+                images = []
+                release_dates = []
+                labels = []
+                artist_ids = []
+                artist_names = []
+                for i in range(0, len(albums)):
+                    names.append(albums[i]['name'])
+                    popularities.append(albums[i]['popularity'])
+                    tracks.append(albums[i]['total_tracks'])
+                    images.append(albums[i]['images'][1]['url'])
+                    release_dates.append(albums[i]['release_date'])
+                    labels.append(albums[i]['label'])
+                    artist_ids.append(albums[i]['artists'][0]['id'])
+                    artist_names.append(albums[i]['artists'][0]['name'])
+
+                album_method = self.sp.albums(res2)
+                albums = album_method['albums']
+
+                for i in range(0, len(albums)):
+                    names.append(albums[i]['name'])
+                    popularities.append(albums[i]['popularity'])
+                    tracks.append(albums[i]['total_tracks'])
+                    images.append(albums[i]['images'][1]['url'])
+                    release_dates.append(albums[i]['release_date'])
+                    labels.append(albums[i]['label'])
+                    artist_ids.append(albums[i]['artists'][0]['id'])
+                    artist_names.append(albums[i]['artists'][0]['name'])
+
+                album_method = self.sp.albums(res3)
+                albums = album_method['albums']
+
+                for i in range(0, len(albums)):
+                    names.append(albums[i]['name'])
+                    popularities.append(albums[i]['popularity'])
+                    tracks.append(albums[i]['total_tracks'])
+                    images.append(albums[i]['images'][1]['url'])
+                    release_dates.append(albums[i]['release_date'])
+                    labels.append(albums[i]['label'])
+                    artist_ids.append(albums[i]['artists'][0]['id'])
+                    artist_names.append(albums[i]['artists'][0]['name'])
+
+            else:
+                album_method = self.sp.albums(album_ids)
+                albums = album_method['albums']
+
+                names = []
+                popularities = []
+                tracks = []
+                images = []
+                release_dates = []
+                labels = []
+                artist_ids = []
+                artist_names = []
+                for i in range(0, len(albums)):
+                    names.append(albums[i]['name'])
+                    popularities.append(albums[i]['popularity'])
+                    tracks.append(albums[i]['total_tracks'])
+                    images.append(albums[i]['images'][1]['url'])
+                    release_dates.append(albums[i]['release_date'])
+                    labels.append(albums[i]['label'])
+                    artist_ids.append(albums[i]['artists'][0]['id'])
+                    artist_names.append(albums[i]['artists'][0]['name'])
+
+            album_info_cols = list(
+                zip(album_ids, names, popularities, tracks, images, release_dates, labels, artist_ids, artist_names))
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            print(e.args)
+
+        return album_info_cols
+
+    def create_album_entity(self):
+
+        try:
+            album_info = self.get_album_data()
+            album_df = pd.DataFrame(album_info,
+                                    columns=['album_id', 'album_name', 'album_popularity', 'album_tracks',
+                                             'album_image', 'album_release_date', 'album_label', 'album_artist_id',
+                                             'album_artist_name'])
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            print(e.args)
+
+        return album_df
--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,21 @@
 from SpotifyETL import SpotifyETL
+import datetime
+import time
+import logging
+
+date_stage = datetime.datetime.today()
+current_date = date_stage.strftime('%m-%d-%Y')
+
+#defining logger conditions
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('	%(asctime)s - %(name)s - %(message)s')
+
+file_handler = logging.FileHandler(f'./logs/{current_date} app.log')
+file_handler.setFormatter(formatter)
+
+start_time = time.time()
 
 #initiate ShopifyETL Class
 s = SpotifyETL()
@@ -8,3 +25,37 @@ print('Getting Recent Songs')
 s.get_recent_songs()
 recent_length = len(s.get_recent_songs())
 print(f'Retrieved {recent_length} songs')
+
+#call get artist ids method
+print('Getting Recent Artists')
+s.get_artist_ids()
+artist_length = len(s.get_artist_ids())
+print(f'Retrieved {artist_length} artists')
+
+#call get album ids method
+print('Getting Recent Albums')
+s.get_album_ids()
+album_length = len(s.get_album_ids())
+print(f'Retrieve {album_length} albums')
+
+#create_Spotify csvs for staging
+print('Creating Track Entity')
+track_entity = s.create_track_entity()
+print('Track Entity Created')
+print('Creating Artist Entity')
+artist_entity = s.create_artist_entity()
+print('Artist Entity Created')
+print('Creating Album Entity')
+album_entity = s.create_album_entity()
+print('Album Entity Created')
+
+print('Storing Entities as CSVs')
+track_entity.to_csv(f'./output/tracks {current_date}.csv')
+artist_entity.to_csv(f'./output/artists {current_date}.csv')
+album_entity.to_csv(f'./output/albums {current_date}.csv')
+
+end_time = time.time()
+
+total_time = end_time - start_time
+
+logger.debug(f'Process took {int(total_time/60)} minutes and {(total_time % 60):.2f} seconds')
